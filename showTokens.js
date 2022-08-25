f00a821d2c4dcb09f1309f384151022df390c901
Cristian Butzke <cristian.btzk@gmail.com>
2022-08-25 10:05:08 -0300

    Add new tokens/show tokens file

---
+++ showTokens.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const nextToken = require('./lexico')
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let token = null;
+  try {
+    do {
+      token = nextToken(data);
+      if (token) {
+        console.log('Token ', token.token, ' - ', token.text, ' - Linha ', token.line, ' - Início/Fim ', token.initialPosition, '/', token.finalPosition)
+      }
+    } while (token);
+  } catch (error) {
+    console.log(error.message)
+  }
+
+});
