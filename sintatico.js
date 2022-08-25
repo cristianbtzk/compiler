da794d5d35dbca1f1e84fe4e7209e5aef6875d64
Cristian Butzke <butzke@DESKTOP-JHT4PDR.localdomain>
2022-08-25 19:31:08 -0300

    Add functions

--- sintatico.js
+++ sintatico.js
@@ -22,10 +22,44 @@
       console.log('a');
   }
 
+  const COMP = () => {
+
+  }
+
+  const ATR = () => {
+
+  }
+
+  const DECL = () => {
+
+  }
+
+  const IF = () => {
+    return term('IF') 
+  }
+
+  const COM = () => {
+    return IF() || DECL() || ATR()
+  }
+
+  // BL = COMANDO & BL | COMANDO
+  // BL = COM BL1
+  // BL1 = VAZIO | COM & BL1
+
+  const BL1 = () => {
+    return VAZIO() || COM() && BL1()
+  }
+
+  const BL = () => {
+    return COM() && BL1()
+  }
+
+  const E1 = () => {
+  }
+
   const E = () => {
     const anterior = cont
-    return term('START') && term('AC') && term('FC')
-    return term('ID') && term('ATR') && term('CONST') && X()
+    return term('START') && BL()
   }
 
   console.log(E())
